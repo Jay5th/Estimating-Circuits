d6d4e1b01aa46aaa17f3b3e05f0deb9132a7f35f
Japheth Varlack <japhethv1229@gmail.com>
2024-05-07 16:54:28 -0400

    Fixed bug in BinaryOperation.py

--- BinaryOperation.py
+++ BinaryOperation.py
@@ -1,24 +1,16 @@
 from dataclasses import dataclass
 
-current_options = {'zero', 'one', 'and', 'or', 'xor'}
+current_options = {'zero': lambda a, b: 0,
+                   'one': lambda a, b: 1,
+                   'and': lambda a, b: a & b,
+                   'or': lambda a, b: a | b,
+                   'xor': lambda a, b: a ^ b}
 
 
 @dataclass(frozen=True)
 class BinaryOperation:
     name: str = 'zero'
-    assert name in current_options, f"I have not implemented a binary operation named {name}."
-    if name == 'zero':
-        def __call__(self, a: int, b: int) -> int:
-            return 0
-    elif name == 'one':
-        def __call__(self, a: int, b: int) -> int:
-            return 1
-    elif name == 'and':
-        def __call__(self, a: int, b: int) -> int:
-            return a & b
-    elif name == 'or':
-        def __call__(self, a: int, b: int) -> int:
-            return a | b
-    elif name == 'xor':
-        def __call__(self, a: int, b: int) -> int:
-            return a ^ b
+    assert name in current_options.keys(), f"I have not implemented a binary operation named {name}."
+
+    def __call__(self, a: int, b: int) -> int:
+        return current_options[self.name](a, b)
