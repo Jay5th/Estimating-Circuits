d0aaf162675ccafb95d4f1a54bd939d366a78d85
Japheth Varlack <japhethv1229@gmail.com>
2024-05-07 18:19:12 -0400

    implemented probability calculation of a boolean circuit

---
+++ boolean_circuit_probability.py
@@ -0,0 +1,11 @@
+from binary_sequences import binary_sequences
+from BooleanCircuit import BooleanCircuit
+
+
+def boolean_circuit_probability(boolean_circuit: BooleanCircuit) -> float:
+    n = boolean_circuit.input_size
+    possible_inputs = binary_sequences(sequence_length=n)
+    one_count = 0
+    for data_input in possible_inputs:
+        one_count += boolean_circuit(data_input)
+    return one_count / 2**n
