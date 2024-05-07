c58c1be1aa675f0c0de0410a1338b64a7b684aff
Japheth Varlack <japhethv1229@gmail.com>
2024-05-07 16:21:20 -0400

    implemented binary gates

--- BinaryGate.py
+++ BinaryGate.py
@@ -1,5 +1,12 @@
 from dataclasses import dataclass
+from BinaryOperation import BinaryOperation
 
-@dataclass
-class Gate:
-    
+
+@dataclass(frozen=True)
+class BinaryGate:
+    indices: tuple[int, int]
+    operation: BinaryOperation
+
+    def __call__(self, data_input: tuple) -> int:
+        a, b = data_input[self.indices[0]], data_input[self.indices[1]]
+        return self.operation(a, b)
