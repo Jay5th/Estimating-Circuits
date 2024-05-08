974231fc1162749bfb677b0dd21b04f384143c1f
Japheth Varlack <japhethv1229@gmail.com>
2024-05-08 15:48:13 -0400

    Renamed a variable in the __call__ function

--- BooleanCircuits/CircuitConstruction/BooleanCircuit.py
+++ BooleanCircuits/CircuitConstruction/BooleanCircuit.py
@@ -6,13 +6,13 @@ class BooleanCircuit:
     input_size: int
     gate_layers: list
 
-    def pass_through_layer(self, layer_input: tuple, layer_index: int) -> tuple:
+    def pass_through_layer(self, data_input: tuple, layer_index: int) -> tuple:
         layer = self.gate_layers[layer_index]
-        return tuple([gate(layer_input) for gate in layer])
+        return tuple([gate(data_input) for gate in layer])
 
     # Forward pass
-    def __call__(self, circuit_input) -> tuple or int:
-        data_stream = tuple(circuit_input)
+    def __call__(self, data_input: tuple) -> tuple or int:
+        data_stream = tuple(data_input)
         for layer_index in range(len(self.gate_layers)):
             data_stream = self.pass_through_layer(data_stream, layer_index)
         if len(data_stream) == 1:
