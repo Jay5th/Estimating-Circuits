fe7e967e40eb8572de94ef4bcaf364cf135f0dcf
Japheth Varlack <japhethv1229@gmail.com>
2024-05-08 15:54:39 -0400

    changed variable name 'radius' to 'node_radius'

--- Visualizations/Binary/create_input_layer.py
+++ Visualizations/Binary/create_input_layer.py
@@ -2,11 +2,11 @@ from Visualizations.Binary.InputNode import InputNode
 import tkinter as tk
 
 
-def create_input_layer(canvas: tk.Canvas, input_size: int, x: int, y: int, radius: int) -> list:
-    center_points_range = 3 * radius * (input_size - 1)
+def create_input_layer(canvas: tk.Canvas, input_size: int, x: int, y: int, node_radius: int) -> list:
+    center_points_range = 3 * node_radius * (input_size - 1)
     left_center_x = x - center_points_range // 2
     input_nodes = []
     for i in range(input_size):
-        xi = left_center_x + 3 * radius * i
-        input_nodes.append(InputNode(canvas=canvas, input_index=i, x=xi, y=y, radius=radius))
+        xi = left_center_x + 3 * node_radius * i
+        input_nodes.append(InputNode(canvas=canvas, input_index=i, x=xi, y=y, radius=node_radius))
     return input_nodes
