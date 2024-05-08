8579085f5c0a8edb3fb7d09a25dd21bd6f13c49c
Japheth Varlack <japhethv1229@gmail.com>
2024-05-08 01:38:06 -0400

    renamed 'current_options' to 'available_options'

--- BooleanCircuits/CircuitConstruction/BinaryOperation.py
+++ BooleanCircuits/CircuitConstruction/BinaryOperation.py
@@ -1,16 +1,16 @@
 from dataclasses import dataclass
 
-current_options = {'zero': lambda a, b: 0,
-                   'one': lambda a, b: 1,
-                   'and': lambda a, b: a & b,
-                   'or': lambda a, b: a | b,
-                   'xor': lambda a, b: a ^ b}
+available_options = {'zero': lambda a, b: 0,
+                     'one': lambda a, b: 1,
+                     'and': lambda a, b: a & b,
+                     'or': lambda a, b: a | b,
+                     'xor': lambda a, b: a ^ b}
 
 
 @dataclass(frozen=True)
 class BinaryOperation:
     name: str = 'zero'
-    assert name in current_options.keys(), f"I have not implemented a binary operation named {name}."
+    assert name in available_options.keys(), f"I have not implemented a binary operation named {name}."
 
     def __call__(self, a: int, b: int) -> int:
-        return current_options[self.name](a, b)
+        return available_options[self.name](a, b)
