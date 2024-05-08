91ebe7473227784ef0f35eece2afc8297cbe5845
Japheth Varlack <japhethv1229@gmail.com>
2024-05-08 15:59:10 -0400

    edited the 'simple circuit' example to include animations

--- Visualizations/Examples/simple_boolean_circuit.py
+++ Visualizations/Examples/simple_boolean_circuit.py
@@ -1,30 +1,76 @@
 import tkinter as tk
-from Visualizations.Binary.create_input_layer import create_input_layer
-from Visualizations.Binary.create_gate_layer import create_gate_layer
-from Visualizations.HelperFunctions.connect_layers import connect_layers
+from BooleanCircuits.CircuitConstruction.BooleanCircuit import BooleanCircuit
+from BooleanCircuits.CircuitConstruction.BinaryGate import BinaryGate
 from BooleanCircuits.CircuitConstruction.BinaryOperation import BinaryOperation
+from Visualizations.Binary.BinaryNetwork import BinaryNetwork
+
 
 root = tk.Tk()
 
 root.geometry("700x700")
-canvas = tk.Canvas(root, width=600, height=600, bg='white')
+
+
+canvas = tk.Canvas(root, width=600, height=600, bg='bisque1')
 canvas.pack()
 
-or_op = BinaryOperation('or')
-and_op = BinaryOperation('and')
-xor_op = BinaryOperation('xor')
+or_op = BinaryOperation(name='or')
+and_op = BinaryOperation(name='and')
+xor_op = BinaryOperation(name='xor')
+
+gate_layers = []
+
+layer0 = [BinaryGate(indices=(0, 1), operation=or_op),
+          BinaryGate(indices=(0, 2), operation=xor_op),
+          BinaryGate(indices=(1, 2), operation=or_op)]
+gate_layers.append(layer0)
+
+layer1 = [BinaryGate(indices=(0, 1), operation=and_op),
+          BinaryGate(indices=(1, 2), operation=and_op)]
+gate_layers.append(layer1)
+
+layer2 = [BinaryGate(indices=(0, 1), operation=xor_op)]
+gate_layers.append(layer2)
+
+simple_boolean_circuit = BooleanCircuit(input_size=3, gate_layers=gate_layers)
+
+simple_network = BinaryNetwork(canvas=canvas, circuit=simple_boolean_circuit, node_radius=40, wire_length=20)
+
+# Data input
+input_label = tk.Label(root, text="Data Input:")
+input_label.place(x=250, y=625)
+
+input_entry = tk.Entry(root, width=25)
+input_entry.place(x=250, y=650)
+
+# Delay input
+delay_label = tk.Label(root, text="Update delay (sec):")
+delay_label.place(x=25, y=625)
+
+delay_entry = tk.Entry(root, width=25)
+delay_entry.place(x=25, y=650)
+
+# Activate button
 
 
-input_layer = create_input_layer(canvas=canvas, input_size=3, x=300, y=50, radius=30)
-layer_1 = create_gate_layer(canvas=canvas, operation_indices_pairs=[(or_op, (0, 1)), (xor_op, (0, 2)), (or_op, (1, 2))],
-                            x=300, y=150, radius=30)
-connect_layers(input_layer, layer_1)
-layer_2 = create_gate_layer(canvas=canvas, operation_indices_pairs=[(and_op, (0, 1)), (and_op, (1, 2))],
-                            x=300, y=250, radius=30)
-connect_layers(layer_1, layer_2)
-layer_3 = create_gate_layer(canvas=canvas, operation_indices_pairs=[(xor_op, (0, 1))],
-                            x=300, y=350, radius=30)
-connect_layers(layer_2, layer_3)
+def activate_command():
+    data_input = eval(input_entry.get())
+    delay = eval(delay_entry.get())
+    simple_network(data_input=data_input, delay=delay)
+
+
+activate_button = tk.Button(root, text="Activate", command=activate_command)
+activate_button.place(x=500, y=625)
+
+
+# Reset button
+
+
+def reset_command():
+    simple_network.reset()
+
+
+reset_button = tk.Button(root, text="Reset", command=reset_command)
+reset_button.place(x=500, y=655)
 
 
 root.mainloop()
