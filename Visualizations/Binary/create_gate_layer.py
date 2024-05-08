14931bc3f24b1a07c1772d5e44901773ae2c66e3
Japheth Varlack <japhethv1229@gmail.com>
2024-05-08 15:53:15 -0400

    changed variable name 'radius' to 'node_radius'. Also, this function now takes binary gates as an input

--- Visualizations/Binary/create_gate_layer.py
+++ Visualizations/Binary/create_gate_layer.py
@@ -3,14 +3,12 @@ from BooleanCircuits.CircuitConstruction.BinaryGate import BinaryGate
 import tkinter as tk
 
 
-def create_gate_layer(canvas: tk.Canvas, operation_indices_pairs: list, x: int, y: int, radius: int) -> list:
-    layer_size = len(operation_indices_pairs)
-    center_points_range = 3 * radius * (layer_size - 1)
+def create_gate_layer(canvas: tk.Canvas, binary_gates: list, x: int, y: int, node_radius: int) -> list:
+    layer_size = len(binary_gates)
+    center_points_range = 3 * node_radius * (layer_size - 1)
     left_center_x = x - center_points_range // 2
     gate_layer = []
     for i in range(layer_size):
-        xi = left_center_x + 3 * radius * i
-        operation, indices = operation_indices_pairs[i]
-        gate_layer.append(BinaryNode(canvas=canvas, gate=BinaryGate(indices=indices, operation=operation),
-                                     x=xi, y=y, radius=radius))
+        xi = left_center_x + 3 * node_radius * i
+        gate_layer.append(BinaryNode(canvas=canvas, gate=binary_gates[i], x=xi, y=y, radius=node_radius))
     return gate_layer
