5da3cee4cebf8a48d2c7fdba8063a55adc037551
Japheth Varlack <japhethv1229@gmail.com>
2024-05-11 16:00:42 -0400

    fixed a bug with type checking

--- ArithmeticCircuits/CircuitConstruction/ArithmeticGate.py
+++ ArithmeticCircuits/CircuitConstruction/ArithmeticGate.py
@@ -7,6 +7,6 @@ class ArithmeticGate:
     indices: tuple[int, int]
     operation: ArithmeticOperation
 
-    def __call__(self, data_input: tuple) -> float:
+    def __call__(self, data_input: tuple[float, ...]) -> float:
         a, b = data_input[self.indices[0]], data_input[self.indices[1]]
         return self.operation(a, b)
