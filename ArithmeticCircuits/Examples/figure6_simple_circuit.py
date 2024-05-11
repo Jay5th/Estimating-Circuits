17fc5ba2c9f4188732cda8f0fe0a14ccde460714
Japheth Varlack <japhethv1229@gmail.com>
2024-05-11 17:08:05 -0400

    added mean progragation calculation to figure 6 example

--- ArithmeticCircuits/Examples/figure6_simple_circuit.py
+++ ArithmeticCircuits/Examples/figure6_simple_circuit.py
@@ -1,6 +1,7 @@
 from ArithmeticCircuits.CircuitConstruction.ArithmeticOperation import ArithmeticOperation
 from ArithmeticCircuits.CircuitConstruction.ArithmeticGate import ArithmeticGate
 from ArithmeticCircuits.CircuitConstruction.ArithmeticCircuit import ArithmeticCircuit
+from ArithmeticCircuits.ProbabilitiesAndEstimates.mean_propagation import mean_propagation
 
 c1 = ArithmeticOperation(name="constant=1")
 add_op = ArithmeticOperation(name="sum")
@@ -23,8 +24,9 @@ layer2 = [ArithmeticGate(indices=(0, 1), operation=multiply_op),
 
 layer3 = [ArithmeticGate(indices=(0, 1), operation=add_op)]
 
-simple_arithmetic_circuit = ArithmeticCircuit(gate_layers=[layer0, layer1, layer2, layer3])
+simple_arithmetic_circuit = ArithmeticCircuit(input_size=3, gate_layers=[layer0, layer1, layer2, layer3])
 
 if __name__ == '__main__':
     data_input = (1, 0, 0)
     print(f"{data_input} -> C ->", simple_arithmetic_circuit(data_input))
+    print("E(C) ~", mean_propagation(circuit=simple_arithmetic_circuit))
