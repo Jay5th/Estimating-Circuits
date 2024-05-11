b07d8f39b61d1ae88bfa9bfffbd00f2b70f26a3c
Japheth Varlack <japhethv1229@gmail.com>
2024-05-11 15:59:48 -0400

    adjusted gate operation so that you can also use it for defining an input gate

--- ArithmeticCircuits/CircuitConstruction/ArithmeticOperation.py
+++ ArithmeticCircuits/CircuitConstruction/ArithmeticOperation.py
@@ -3,11 +3,18 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class ArithmeticOperation:
-    name: str = 'constant=0'
+    name: str = 'first'
 
     def __call__(self, a: float, b: float) -> float:
         if self.name == 'sum':
             return a + b
         if self.name == 'product':
             return a * b
-        return float(self.name[9:])
+        if self.name[0:8] == 'constant':
+            return float(self.name[9:])
+        if self.name == 'first':
+            return a
+        if self.name == 'last':
+            return b
+        else:
+            raise ValueError(f"an arithmetic operation named {self.name} has not been implemented")
